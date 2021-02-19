80bafdd089f3edf358ee731cb8f6c81e97b47aec
XDEDM314 <78266147+XDEDM314@users.noreply.github.com>
2021-02-19 22:41:43 +0800

    Update game.h

--- game.h
+++ game.h
@@ -1,1 +1,7 @@
-
+#define row 32
+#define lin 32
+void start();
+void initialize(char board[row][lin]);
+void display(char board[row][lin]);
+int iswrong(char board[row][lin],int x,int y);
+int iswin(char board[row][lin], int x, int y);
